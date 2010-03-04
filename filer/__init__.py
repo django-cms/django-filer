75d00d5b468011b4af66320f07f34b1e542f2514
stefanfoulis <stefan.foulis@gmail.com>
2010-03-04 15:51:36 +0100

    closes #6: Add a "devide_by" template filter (image resizing)

--- filer/__init__.py
+++ filer/__init__.py
@@ -1,6 +1,6 @@
 """
 django-filer - file and image management for django projects
 """
-VERSION = (0,6,2,'a5')
-__version__ = "0.6.2a5"
+VERSION = (0,6,2,'a6')
+__version__ = "0.6.2a6"
 __authors__ = ["Stefan Foulis <stefan.foulis@gmail.com>", ]
--- filer/templatetags/filermedia.py
+++ filer/templatetags/filermedia.py
@@ -17,33 +17,39 @@ def filer_staticmedia_prefix():
 filer_staticmedia_prefix = register.simple_tag(filer_staticmedia_prefix)
 
 
-def _recalculate_size(size, padding, index):
-    new_one = size[index] - padding
+def _recalculate_size(size, index, divisor=0, padding=0):
+    new_one = size[index]
+    if divisor:
+        new_one = new_one / float(divisor)
+    if padding:
+        new_one = new_one - padding
     new_two = int(float(new_one)*float(size[int(not index)])/size[index])
+    new_one = int(new_one)
     if index:
         return (new_two, new_one)
     return new_one, new_two
 
-def _extra_padding(original_size, padding, index):
+
+def _resize(original_size, index, divisor=0, padding=0):
     if not SIZE_RE.match(original_size):
         return original_size
     else:
         original_size = [int(part) for part in original_size.split('x')]
     try:
         padding = int(padding)
+        divisor = int(divisor)
     except (TypeError, ValueError):
         return original_size
     # Re-calculate size
-    new_x, new_y = _recalculate_size(original_size, padding, index)
+    new_x, new_y = _recalculate_size(original_size, index, divisor=divisor, padding=padding)
     return '%sx%s' % (new_x, new_y)
-
 
 def extra_padding_x(original_size, padding):
     """
     Reduce the width of `original_size` by `padding`, maintaining the aspect
     ratio.
     """
-    return _extra_padding(original_size, padding, 0)
+    return _resize(original_size, 0, padding=padding)
 extra_padding_x = register.filter(extra_padding_x)
 
 def extra_padding_y(original_size, padding):
@@ -51,5 +57,19 @@ def extra_padding_y(original_size, padding):
     Reduce the height of `original_size` by `padding`, maintaining the aspect
     ratio.
     """
-    return _extra_padding(original_size, padding, 1)
+    return _resize(original_size, 1, padding=padding)
 extra_padding_y = register.filter(extra_padding_y)
+
+def devide_x_by(original_size, divisor):
+    """
+    Reduce by half
+    """
+    return _resize(original_size, 0, divisor=divisor)
+devide_x_by = register.filter(devide_x_by)
+
+def devide_y_by(original_size, divisor):
+    """
+    Reduce by half
+    """
+    return _resize(original_size, 1, divisor=divisor)
+devide_y_by = register.filter(devide_y_by)
