33ce8cb5fa773e556ea61932d01832a852a8ce88
stefanfoulis <stefan.foulis@gmail.com>
2010-03-05 14:11:37 +0100

    fixed version number to 0.6.2a9

--- filer/__init__.py
+++ filer/__init__.py
@@ -1,6 +1,6 @@
 """
 django-filer - file and image management for django projects
 """
-VERSION = (0,6,2,'a8')
-__version__ = "0.6.2a8"
+VERSION = (0,6,2,'a9')
+__version__ = "0.6.2a9"
 __authors__ = ["Stefan Foulis <stefan.foulis@gmail.com>", ]
