0b0097583c2a559f23752d5dc06c66866194c73a
stefanfoulis <stefan.foulis@gmail.com>
2010-03-15 16:42:12 +0100

    remove link to custom jquery. replace raphael.js with newer version

--- filer/admin/imageadmin.py
+++ filer/admin/imageadmin.py
@@ -33,7 +33,6 @@ class ImageAdminFrom(forms.ModelForm):
             #'all': (settings.MEDIA_URL + 'filer/css/focal_point.css',)
         }
         js = (
-            settings.MEDIA_URL + 'filer/js/jquery-1.3.2.min.js',
             settings.MEDIA_URL + 'filer/js/raphael.js',
             settings.MEDIA_URL + 'filer/js/focal_point.js',
         )
