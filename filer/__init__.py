04bc6039c8ec6f47079732b97585f137fb4ef999
stefanfoulis <stefan.foulis@gmail.com>
2010-03-31 23:17:57 +0200

    fixes #35: make files replaceable (version bump to 0.6.2a11)

--- filer/__init__.py
+++ filer/__init__.py
@@ -1,6 +1,6 @@
 """
 django-filer - file and image management for django projects
 """
-VERSION = (0,6,2,'a10')
-__version__ = "0.6.2a10"
+VERSION = (0,6,2,'a11')
+__version__ = "0.6.2a11"
 __authors__ = ["Stefan Foulis <stefan.foulis@gmail.com>", ]
