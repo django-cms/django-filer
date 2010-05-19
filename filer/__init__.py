c40fb04466e89d6ca75f39c759be73a2758cef47
stefanfoulis <stefan.foulis@gmail.com>
2010-05-19 17:46:13 +0200

    version bump to 0.6.2a16

--- filer/__init__.py
+++ filer/__init__.py
@@ -1,6 +1,6 @@
 """
 django-filer - file and image management for django projects
 """
-VERSION = (0,6,2,'a15')
-__version__ = "0.6.2a15"
+VERSION = (0,6,2,'a16')
+__version__ = "0.6.2a16"
 __authors__ = ["Stefan Foulis <stefan.foulis@gmail.com>", ]
