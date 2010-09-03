bfdce078360bc3366b8ee70d8f30e5033830d2bc
Yann Malet <yann.malet@gmail.com>
2010-09-03 18:57:12 +0200

    pylint loves for fields/image.py

--- filer/fields/image.py
+++ filer/fields/image.py
@@ -1,13 +1,3 @@
-from django.utils.translation import ugettext as _
-from django.utils.text import truncate_words
-from django.utils import simplejson
-from django.db import models
-from django import forms
-from django.contrib.admin.widgets import ForeignKeyRawIdWidget
-from django.core.urlresolvers import reverse
-from django.utils.safestring import mark_safe
-from filer.settings import FILER_STATICMEDIA_PREFIX
-from django.conf import settings as globalsettings
 from filer.fields.file import AdminFileWidget, AdminFileFormField, FilerFileField
 from filer.models import Image
 
