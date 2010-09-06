35975d0974530154b599cc3bc165f36bddc49019
stefanfoulis <stefan.foulis@gmail.com>
2010-09-06 13:20:27 +0200

    fixed Issue #40: "Create Folder Popup does not close", version bump to 0.6.2a17

--- filer/__init__.py
+++ filer/__init__.py
@@ -1,6 +1,6 @@
 """
 django-filer - file and image management for django projects
 """
-VERSION = (0,6,2,'a16')
-__version__ = "0.6.2a16"
+VERSION = (0,6,2,'a17')
+__version__ = "0.6.2a17"
 __authors__ = ["Stefan Foulis <stefan.foulis@gmail.com>", ]
