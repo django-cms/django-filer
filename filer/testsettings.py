4b5ae40535091dd8f0e14aceee8ab45db1282781
stefanfoulis <stefan.foulis@gmail.com>
2010-11-28 17:08:07 +0100

    update buildout settings to work for running tests

--- filer/testsettings.py
+++ filer/testsettings.py
@@ -1,4 +1,11 @@
+import os
+PACKAGE_ROOT = os.path.abspath( os.path.join(os.path.dirname(__file__), '../') )
+PROJECT_ROOT = os.path.join(PACKAGE_ROOT, 'filer')
+TMP_ROOT = os.path.abspath( os.path.join(PACKAGE_ROOT, 'tmp') )
 DATABASE_ENGINE = 'sqlite3'
-DATABASE_NAME = '/tmp/filer.db' # won't actually be used. tests under SQLite are run in-memory
-INSTALLED_APPS = ['filer']
-ROOT_URLCONF = ['filer.urls']
+DATABASE_NAME = os.path.join(TMP_ROOT,'filer_test.db') # won't actually be used. tests under SQLite are run in-memory
+INSTALLED_APPS = ['filer','mptt','django.contrib.auth','django.contrib.contenttypes','easy_thumbnails']
+ROOT_URLCONF = []
+
+MEDIA_ROOT = os.path.abspath( os.path.join(TMP_ROOT, 'media') )
+print "MEDIA_ROOT: ", MEDIA_ROOT
