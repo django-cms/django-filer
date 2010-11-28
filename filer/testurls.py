d6b09b93ff0ae4855efb3e79ecfa90a100f9b5a0
stefanfoulis <stefan.foulis@gmail.com>
2010-11-28 17:18:12 +0100

    updated testsettings.pt with minimal stuff to make the testsuite run

---
+++ filer/testurls.py
@@ -0,0 +1,9 @@
+from django.conf.urls.defaults import *
+from django.conf import settings
+from django.contrib import admin
+
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    (r'^admin/', include(admin.site.urls)),
+)
