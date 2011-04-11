923c5c30762f2355545ea67e73e7d1fe34cbd6b7
Brandon Konkle <brandon.konkle@gmail.com>
2011-04-11 14:34:16 -0700

    Only add slashes if there is a value for the file or thumbnail prefixes

--- filer/server/urls.py
+++ filer/server/urls.py
@@ -2,14 +2,22 @@
 from django.conf.urls.defaults import patterns, url
 from filer import settings as filer_settings
 
+# Only add slashes if there is a value for the file or thumbnail prefixes
+if filer_settings.FILER_PRIVATEMEDIA_FILE_URL_PREFIX:
+    file_prefix = filer_settings.FILER_PRIVATEMEDIA_FILE_URL_PREFIX + "/"
+else:
+    file_prefix = ''
+if filer_settings.FILER_PRIVATEMEDIA_THUMBNAIL_URL_PREFIX:
+    thumb_prefix = filer_settings.FILER_PRIVATEMEDIA_THUMBNAIL_URL_PREFIX + "/"
+else:
+    thumb_prefix = ''
+
 urlpatterns = patterns('filer.server.views',
     # This url is generated in filer.models.filemodels.url
-    url(r'^' + filer_settings.FILER_PRIVATEMEDIA_FILE_URL_PREFIX + r'/(?P<path>.*)$',
-        'serve_protected_file',),
+    url(r'^' + file_prefix + r'(?P<path>.*)$', 'serve_protected_file',),
 
 #    # This url is generated in filer.templatetags.filer_image_tags.WrapPrivateThumbnailFile
-    url(r'^' + filer_settings.FILER_PRIVATEMEDIA_THUMBNAIL_URL_PREFIX + r'/(?P<path>.*)$',
-        'serve_protected_thumbnail',),
+    url(r'^' + thumb_prefix + r'(?P<path>.*)$', 'serve_protected_thumbnail',),
 )
 
 #if static_server != None and not static_server.allowsDirectAccess:
