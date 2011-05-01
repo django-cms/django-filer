e6be1251aaded919f34f54baf929f8e61b1755eb
stefanfoulis <stefan.foulis@gmail.com>
2011-05-01 22:06:53 +0200

    made permission edit view a little bit nicer (by far not good enough yet to consider #81 as closed)

---
+++ filer/admin/permissionadmin.py
@@ -0,0 +1,13 @@
+#-*- coding: utf-8 -*-
+from django.contrib import admin
+
+class PermissionAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (None, {'fields': (('type', 'folder',))}),
+        (None, {'fields': (('user', 'group', 'everybody'),)}),
+        (None, {'fields': (
+                    ('can_edit','can_read','can_add_children')
+                    )}
+        ),
+    )
+    raw_id_fields = ('folder', 'user', 'group',)
