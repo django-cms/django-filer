ec6120054d6312cf27d325fac5b8bef164640e4d
stefanfoulis <stefan.foulis@gmail.com>
2011-05-16 21:01:20 +0200

    pep8 love

--- filer/__init__.py
+++ filer/__init__.py
@@ -2,6 +2,6 @@
 """
 django-filer - file and image management for django projects
 """
-VERSION = (0,8,2)
+VERSION = (0, 8, 2)
 __version__ = "0.8.2"
 __authors__ = ["Stefan Foulis <stefan.foulis@gmail.com>", ]
