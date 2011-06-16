743165cd48c268cc061736f084eac3621172bd8f
daveb <daveb@daveb-lintop.(none)>
2011-06-16 13:57:54 +0000

    Added support for lighttpd

--- filer/server/backends/xsendfile.py
+++ filer/server/backends/xsendfile.py
@@ -7,5 +7,12 @@ class ApacheXSendfileServer(ServerBase):
     def serve(self, request, file, **kwargs):
         response = HttpResponse()
         response['X-Sendfile'] = file.path
+
+	# This is needed for lighttpd, hopefully this will 
+        # not be needed after this is fixed:
+	# http://redmine.lighttpd.net/issues/2076
+        response['Content-Type'] = self.get_mimetype(file.path)
+
         self.default_headers(request=request, response=response, file=file, **kwargs)
         return response
+
