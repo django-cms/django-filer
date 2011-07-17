fa2b2e2dfb3d20a6018e270089afaf29e7d6b82f
stefanfoulis <stefan.foulis@gmail.com>
2011-07-17 17:53:22 +0200

    nicer permission admin

--- filer/admin/__init__.py
+++ filer/admin/__init__.py
@@ -5,12 +5,12 @@ from filer.admin.clipboardadmin import ClipboardAdmin
 from filer.admin.fileadmin import FileAdmin
 from filer.admin.folderadmin import FolderAdmin
 from filer.admin.imageadmin import ImageAdmin
-#from filer.admin.permissionadmin import PermissionAdmin
+from filer.admin.permissionadmin import PermissionAdmin
 from filer.models import Folder, File, Clipboard, Image
+from filer.models.permissionmodels import Permission
 
 if settings.FILER_ENABLE_PERMISSIONS:
-    pass
-#    admin.site.register(FolderPermission, PermissionAdmin)
+    admin.site.register(Permission, PermissionAdmin)
 admin.site.register(Folder, FolderAdmin)
 admin.site.register(File, FileAdmin)
 admin.site.register(Clipboard, ClipboardAdmin)
--- filer/admin/permissionadmin.py
+++ filer/admin/permissionadmin.py
@@ -4,18 +4,45 @@ from filer.fields import folder
 
 
 class PermissionAdmin(admin.ModelAdmin):
+    list_display = ('edit', 'who_combined', 'subject_combined', 'can', 'is_inheritable',)
+    list_filter = ('who', 'subject', 'can', 'is_inheritable',)
+    search_fields = ('user__username', 'user__first_name', 'user__last_name', 'user__email',
+                     'group__name',)
     fieldsets = (
-        (None, {'fields': (('type', 'folder',))}),
-        (None, {'fields': (('user', 'group', 'everybody'),)}),
+        (None, {'fields': ('who', ('user', 'group',))}),
+        (None, {'fields': ('subject', ('folder', 'file',),)}),
         (None, {'fields': (
-                    ('can_edit', 'can_read', 'can_add_children')
+                    ('can', 'is_inheritable', ),
                     )}
         ),
     )
-    raw_id_fields = ('user', 'group',)
+    raw_id_fields = ('user', 'group', 'file')
+
+    def edit(self, obj):
+        return 'edit'
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         db = kwargs.get('using')
         if db_field.name == 'folder':
             kwargs['widget'] = folder.AdminFolderWidget(db_field.rel, using=db)
         return super(PermissionAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
+
+    def who_combined(self, obj):
+        if obj.who == 'user':
+            return u'<strong>%s</strong> (User)' % obj.user
+        elif obj.who == 'group':
+            return u'<strong>%s</strong> (Group)' % obj.group
+        return u'<strong>%s</strong>' % obj.get_who_display()
+    who_combined.allow_tags = True
+    who_combined.short_description = 'who'
+    who_combined.admin_order_field = 'who'
+
+    def subject_combined(self, obj):
+        if obj.subject == 'file':
+            return u'<strong>%s</strong> (File)' % obj.file
+        elif obj.subject == 'folder':
+            return u'<strong>%s</strong> (Folder)' % obj.folder
+        return u'<strong>%s</strong>' % obj.get_subject_display()
+    subject_combined.allow_tags = True
+    subject_combined.short_description = 'subject'
+    subject_combined.admin_order_field = 'subject'
