b89dcb576d01b64a115fe31bf4bbde1b3b8a01b8
stefanfoulis <stefan.foulis@gmail.com>
2011-08-28 01:54:35 +0200

    removed some unused imports

--- filer/admin/clipboardadmin.py
+++ filer/admin/clipboardadmin.py
@@ -1,10 +1,7 @@
 #-*- coding: utf-8 -*-
-from django.core.exceptions import ValidationError
 from django.forms.models import modelform_factory
 from django.contrib import admin
 from django.http import HttpResponse
-from django.shortcuts import render_to_response
-from django.template import RequestContext
 from django.utils import simplejson
 from django.views.decorators.csrf import csrf_exempt
 from filer import settings as filer_settings
