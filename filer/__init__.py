7d1614f0e13960fa732f5ba8d8ab102403c75cb3
stefanfoulis <stefan.foulis@gmail.com>
2011-08-28 01:02:46 +0200

    nicer __version__

--- filer/__init__.py
+++ filer/__init__.py
@@ -2,6 +2,21 @@
 """
 django-filer - file and image management for django projects
 """
-VERSION = (0, 8, 2)
-__version__ = "0.8.2"
-__authors__ = ["Stefan Foulis <stefan.foulis@gmail.com>", ]
+__version_info__ = {
+    'major': 0,
+    'minor': 8,
+    'micro': 2,
+    'releaselevel': 'final',
+    'serial': 1
+}
+
+def get_version(short=False):
+    assert __version_info__['releaselevel'] in ('alpha', 'beta', 'final')
+    vers = ["%(major)i.%(minor)i" % __version_info__, ]
+    if __version_info__['micro']:
+        vers.append(".%(micro)i" % __version_info__)
+    if __version_info__['releaselevel'] != 'final' and not short:
+        vers.append('%s%i' % (__version_info__['releaselevel'][0], __version_info__['serial']))
+    return ''.join(vers)
+
+__version__ = get_version()
