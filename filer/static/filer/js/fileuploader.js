3264d88ab9a6db6104db6c8c4598c228686f64a9
stefanfoulis <stefan.foulis@gmail.com>
2011-09-18 21:57:08 +0200

    update LICENSE information
    * switched to MIT licensed version of fileuploader.js (Thank you @valums :-)
    * added LICENSES overview file that lists the various licenses of bundled software
    * actually include the correct license (was MIT instead of BSD)

--- filer/static/filer/js/fileuploader.js
+++ filer/static/filer/js/fileuploader.js
@@ -1,11 +1,27 @@
-/**
- * http://github.com/valums/file-uploader
- * 
- * Multiple file upload component with progress-bar, drag-and-drop. 
- * © 2010 Andrew Valums ( andrew(at)valums.com ) 
- * 
- * Licensed under GNU GPL 2 or later and GNU LGPL 2 or later, see license.txt.
- */    
+/*
+Multiple file upload component with progress-bar, drag-and-drop.
+http://github.com/valums/file-uploader
+
+Copyright (C) 2011 by Andris Valums
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
 
 //
 // Helper functions
