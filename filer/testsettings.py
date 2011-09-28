022df924879ad8566e432d284632ba167b7da621
stefanfoulis <stefan.foulis@gmail.com>
2011-09-28 22:48:07 +0200

    buildout and test settings updates

--- filer/testsettings.py
+++ filer/testsettings.py
@@ -4,7 +4,7 @@ PACKAGE_ROOT = os.path.abspath( os.path.join(os.path.dirname(__file__), '../') )
 PROJECT_ROOT = os.path.join(PACKAGE_ROOT, 'filer')
 TMP_ROOT = os.path.abspath( os.path.join(PACKAGE_ROOT, 'tmp') )
 DATABASE_ENGINE = 'sqlite3'
-DATABASE_NAME = os.path.join(TMP_ROOT,'filer_test.db') # won't actually be used. tests under SQLite are run in-memory
+DATABASE_NAME = 'db.sqlite3'
 INSTALLED_APPS = [
     'filer',
     'mptt',
@@ -16,3 +16,4 @@ INSTALLED_APPS = [
 ROOT_URLCONF = 'filer.testurls'
 
 MEDIA_ROOT = os.path.abspath( os.path.join(TMP_ROOT, 'media') )
+MEDIA_URL = '/media/'
