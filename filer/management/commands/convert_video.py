d4fc60a3c8a05043915f3e73a53f4977a22635e5
Nuno Maltez <nuno@cognitiva.com>
2011-10-25 12:37:00 +0100

    Added remaining files (still learning how to use git)

---
+++ filer/management/commands/convert_video.py
@@ -0,0 +1,23 @@
+from django.core.management.base import BaseCommand, CommandError
+from filer.models.videomodels import Video
+
+class Command(BaseCommand):
+    args = ''
+    help = 'process next video conversion task'
+
+    def handle(self, *args, **options):
+        try:
+            vid = Video.objects.filter(status='new')[0:1].get()
+        except Video.DoesNotExist:
+            self.stdout.write("No more tasks\n")
+            return
+        vid.status = 'process'
+        vid.save()
+        result, output = vid.convert()
+        if result:
+            vid.status = 'error'
+        else:
+            vid.status = 'ok'
+        vid.output = output
+        vid.save()
+        self.stdout.write("Processed %s\n" % vid.file.name)
