f8c76e1d124695ac6fded30e3a0742cfcfbfa165
Stefan Foulis <stefan@foulis.ch>
2012-06-28 14:34:38 +0200

    add warning about installing packages

--- runtests.sh
+++ runtests.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
+read -r -p "This will install dependencies and run the testsuite. Please make sure you are in a virtualenv! Continue? [Y/n]" response
+case $response in
+	[yY]|[eE]|[sS]|[yY])
+		;;
+	*)
+		echo "cancelled"
+		exit
+		;;
+esac
+
 find . -name '*.pyc' -delete
 export DJANGO_VERSION="1.3.1"
 ./.travis_setup
 python setup.py test
+
+#find . -name '*.pyc' -delete
+#export DJANGO_VERSION="1.4"
+#./.travis_setup
+#python setup.py test
