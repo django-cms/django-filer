a54c99bb0146033fd25ff3dff3cdbcdb3b5edaaf
Stefan Foulis <stefan@foulis.ch>
2012-07-04 15:37:57 +0200

    fix url import to work in django<1.4

--- filer/server/urls.py
+++ filer/server/urls.py
@@ -1,5 +1,5 @@
 #-*- coding: utf-8 -*-
-from django.conf.urls import patterns, url, include
+from django.conf.urls.defaults import patterns, url, include
 from filer import settings as filer_settings
 
 urlpatterns = patterns('filer.server.views',
