29e3512fb600fc71e244b4168c8b65132f7bb63a
Stefan Foulis <stefan@foulis.ch>
2012-07-04 09:12:10 +0200

    divide server urls for easier custom including

--- filer/server/thumbnails_server_urls.py
+++ filer/server/thumbnails_server_urls.py
@@ -2,6 +2,5 @@
 from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('filer.server.views',
-    url(r'^(?P<path>.*)$',
-        'serve_protected_file',),
+    url(r'^(?P<path>.*)$', 'serve_protected_thumbnail',),
 )
--- filer/server/urls.py
+++ filer/server/urls.py
@@ -1,11 +1,8 @@
 #-*- coding: utf-8 -*-
-from django.conf.urls.defaults import patterns, url
+from django.conf.urls import patterns, url, include
 from filer import settings as filer_settings
 
 urlpatterns = patterns('filer.server.views',
-    url(r'^' + filer_settings.FILER_PRIVATEMEDIA_STORAGE.base_url.lstrip('/') + r'(?P<path>.*)$',
-        'serve_protected_file',),
-
-    url(r'^' + filer_settings.FILER_PRIVATEMEDIA_THUMBNAIL_STORAGE.base_url.lstrip('/') + r'(?P<path>.*)$',
-        'serve_protected_thumbnail',),
+    url(r'^' + filer_settings.FILER_PRIVATEMEDIA_STORAGE.base_url.lstrip('/'), include('filer.server.main_server_urls')),
+    url(r'^' + filer_settings.FILER_PRIVATEMEDIA_THUMBNAIL_STORAGE.base_url.lstrip('/'), include('filer.server.thumbnails_server_urls')),
 )
