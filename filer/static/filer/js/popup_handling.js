6235493ef38b533ad51d4d566453dd1369aa27c6
Iacopo Spalletti <i.spalletti@nephila.it>
2012-07-07 03:40:44 +0200

    Debug code removed

--- filer/static/filer/js/popup_handling.js
+++ filer/static/filer/js/popup_handling.js
@@ -18,10 +18,8 @@
 	dismissRelatedFolderLookupPopup = function(win, chosenId, chosenName) {
 		var id = windowname_to_id(win.name);
 		var id_name = id + '_description_txt';
-        alert(id);
-        alert(id_name);
 		document.getElementById(id).value = chosenId;
 		document.getElementById(id_name).innerHTML = chosenName;
-		//win.close();
+		win.close();
 	};
 })(jQuery);
