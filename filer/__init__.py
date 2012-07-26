e2c92c32332e51aeca990061e173ab79be634d0e
Stefan Foulis <stefan@foulis.ch>
2012-07-26 11:48:51 +0200

    version bump to 0.8.7

--- filer/__init__.py
+++ filer/__init__.py
@@ -1,6 +1,6 @@
 """
 django-filer - file and image management for django projects
 """
-VERSION = (0,8,6)
-__version__ = "0.8.6"
+VERSION = (0,8,7)
+__version__ = "0.8.7"
 __authors__ = ["Stefan Foulis <stefan.foulis@gmail.com>", ]
