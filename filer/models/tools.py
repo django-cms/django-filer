59c75ddb9e82ae77059aaf0cb3dced8a4e259c63
Alex Cucu <alexandruioan.cucu@gmail.com>
2013-01-23 19:14:03 +0200

    Added a setting for stating whether UPLOAD_TO callack depends on the folder where the file is ment to be uploaded

--- filer/models/tools.py
+++ filer/models/tools.py
@@ -17,13 +17,30 @@ def get_user_clipboard(user):
         clipboard = Clipboard.objects.get_or_create(user=user)[0]
         return clipboard
 
+def move_file(file_obj, folder):
+    file_obj.folder = folder
+    if file_obj.is_public:
+        upload_depends_on_folder = \
+            filer_settings.FILER_PUBLICMEDIA_UPLOAD_DEPENDS_ON_FOLDER
+        upload_to = filer_settings.FILER_PUBLICMEDIA_UPLOAD_TO
+    else:
+        upload_depends_on_folder = \
+            filer_settings.FILER_PRIVATEMEDIA_UPLOAD_DEPENDS_ON_FOLDER
+        upload_to = filer_settings.FILER_PRIVATEMEDIA_UPLOAD_TO
+    if upload_depends_on_folder:
+        storage = file_obj.file.storage
+        new_location = upload_to(file_obj, file_obj.original_filename)
+        new_name = storage.save(new_location, file_obj.file.file)
+        storage.delete(file_obj.file.name)
+        file_obj.file.name = new_name
+    file_obj.save()
+
 
 def move_file_to_clipboard(files, clipboard):
     count = 0
     for file_obj in files:
         if clipboard.append_file(file_obj):
-            file_obj.folder = None
-            file_obj.save()
+            move_file(file_obj, None)
             count += 1
     return count
 
@@ -34,16 +51,5 @@ def move_files_from_clipboard_to_folder(clipboard, folder):
 
 def move_files_to_folder(files, folder):
     for file_obj in files:
-        if file_obj.is_public:
-            upload_to = filer_settings.FILER_PUBLICMEDIA_UPLOAD_TO
-        else:
-            upload_to = filer_settings.FILER_PRIVATEMEDIA_UPLOAD_TO
-
-        storage = file_obj.file.storage
-        file_obj.folder = folder
-        new_location = upload_to(file_obj, file_obj.original_filename)
-        new_name = storage.save(new_location, file_obj.file.file)
-        storage.delete(file_obj.file.name)
-        file_obj.file.name = new_name
-        file_obj.save()
+        move_file(file_obj, folder)
     return True
