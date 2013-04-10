1ba9a5a2cde2f383463de749c8cd53a8fa3b5e3f
Zoltan Szeredi <zoltan.szeredi@3pillarglobal.com>
2013-04-10 09:25:48 +0300

    Improve the performance of directory listing.
    Performance is improved by no loger executing a query for each file and
    folder. Now a single query is executed for gathering all of a user's
    permitted ids.
    
    The folder_qs and file_qs QuerySets filter on the user's permitted ids
    => just 3 queries required (instead of one for each file/folder)

--- filer/admin/folderadmin.py
+++ filer/admin/folderadmin.py
@@ -252,6 +252,50 @@ class FolderAdmin(PrimitivePermissionAwareModelAdmin):
             except ValueError:
                 pass
 
+        def filter_folder(qs, terms=[]):
+            for term in terms:
+                qs = qs.filter(Q(name__icontains=term) | \
+                               Q(owner__username__icontains=term) | \
+                               Q(owner__first_name__icontains=term) | \
+                               Q(owner__last_name__icontains=term))
+            return qs
+
+        def filter_file(qs, terms=[]):
+            for term in terms:
+                qs = qs.filter(Q(name__icontains=term) | \
+                               Q(description__icontains=term) | \
+                               Q(original_filename__icontains=term) | \
+                               Q(owner__username__icontains=term) | \
+                               Q(owner__first_name__icontains=term) | \
+                               Q(owner__last_name__icontains=term))
+            return qs
+
+        def get_user_read_permissions(user):
+            if not user.is_authenticated():
+                return None
+            elif user.is_superuser:
+                return 'All'
+            else:
+                permission_manager = FolderPermission.objects
+                read_permissions = permission_manager.get_read_id_list(user)
+                return read_permissions
+
+        def _filter_generic_permission(qs, permissions, entry_type):
+            if permissions == 'All':
+                return qs
+
+            if entry_type == 'folder':
+                qs = qs.filter(Q(id__in=permissions)|Q(owner=request.user))
+            elif entry_type == 'file':
+                qs = qs.filter(Q(folder__id__in=permissions)|Q(owner=request.user))
+            return qs
+
+        def filter_folder_permission(qs, permissions):
+            return _filter_generic_permission(qs, permissions, 'folder')
+
+        def filter_file_permission(qs, permissions):
+            return _filter_generic_permission(qs, permissions, 'file')
+
         # search
         q = request.GET.get('q', None)
         if q:
@@ -286,24 +330,14 @@ class FolderAdmin(PrimitivePermissionAwareModelAdmin):
         folder_files = []
         if folder.is_root:
             folder_children += folder.virtual_folders
-        for f in folder_qs:
-            f.perms = userperms_for_request(f, request)
-            if hasattr(f, 'has_read_permission'):
-                if f.has_read_permission(request):
-                    folder_children.append(f)
-                else:
-                    pass
-            else:
-                folder_children.append(f)
-        for f in file_qs:
-            f.perms = userperms_for_request(f, request)
-            if hasattr(f, 'has_read_permission'):
-                if f.has_read_permission(request):
-                    folder_files.append(f)
-                else:
-                    pass
-            else:
-                folder_files.append(f)
+
+        user = request.user
+        permissions = get_user_read_permissions(user)
+        folder_qs = filter_folder_permission(folder_qs, permissions)
+        file_qs = filter_file_permission(file_qs, permissions)
+        folder_children += folder_qs
+        folder_files += file_qs
+
         try:
             permissions = {
                 'has_edit_permission': folder.has_edit_permission(request),
