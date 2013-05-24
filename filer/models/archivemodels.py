713ec605093a35c02318f2bb2e16110e6a48ff70
Zoltan Szeredi <zoltan.szeredi@3pillarglobal.com>
2013-05-24 19:04:45 +0300

    LUN-733: Create filer-file properly using their subtype.

--- filer/models/archivemodels.py
+++ filer/models/archivemodels.py
@@ -4,7 +4,9 @@ Archiving support for filer.
 from filer.models.filemodels import File, Folder
 from django.utils.translation import ugettext_lazy as _
 from django.core.files.base import ContentFile
-from filer.settings import FILER_IS_PUBLIC_DEFAULT
+from filer.settings import FILER_IS_PUBLIC_DEFAULT, FILER_FILE_MODELS
+from filer.utils.loader import load_object
+
 
 import os.path
 import zipfile
@@ -123,7 +125,11 @@ class Archive(File):
         """Helper wrapper of creating a filer file."""
         file_data = ContentFile(data)
         file_data.name = basename
-        file_object, created = File.objects.get_or_create(
+        for filer_class in FILER_FILE_MODELS:
+            FileSubClass = load_object(filer_class) 
+            if FileSubClass.matches_file_type(basename, None, None):
+                break
+        file_object, created = FileSubClass.objects.get_or_create(
             original_filename=file_data.name,
             folder=folder,
             owner=self.owner,
