18174a59dfd3325cb664a85c24da5ae19dbf51d8
Alex Cucu <alexandruioan.cucu@gmail.com>
2013-07-25 17:59:05 +0300

    Remove useless nested functions and unused functions

--- filer/admin/folderadmin.py
+++ filer/admin/folderadmin.py
@@ -252,24 +252,6 @@ class FolderAdmin(PrimitivePermissionAwareModelAdmin):
             except ValueError:
                 pass
 
-        def filter_folder(qs, terms=[]):
-            for term in terms:
-                qs = qs.filter(Q(name__icontains=term) | \
-                               Q(owner__username__icontains=term) | \
-                               Q(owner__first_name__icontains=term) | \
-                               Q(owner__last_name__icontains=term))
-            return qs
-
-        def filter_file(qs, terms=[]):
-            for term in terms:
-                qs = qs.filter(Q(name__icontains=term) | \
-                               Q(description__icontains=term) | \
-                               Q(original_filename__icontains=term) | \
-                               Q(owner__username__icontains=term) | \
-                               Q(owner__first_name__icontains=term) | \
-                               Q(owner__last_name__icontains=term))
-            return qs
-
         def get_user_read_permissions(user):
             if not user.is_authenticated():
                 return None
@@ -279,22 +261,6 @@ class FolderAdmin(PrimitivePermissionAwareModelAdmin):
                 permission_manager = FolderPermission.objects
                 read_permissions = permission_manager.get_read_id_list(user)
                 return read_permissions
-
-        def _filter_generic_permission(qs, permissions, entry_type):
-            if permissions == 'All':
-                return qs
-
-            if entry_type == 'folder':
-                qs = qs.filter(Q(id__in=permissions)|Q(owner=request.user))
-            elif entry_type == 'file':
-                qs = qs.filter(Q(folder__id__in=permissions)|Q(owner=request.user))
-            return qs
-
-        def filter_folder_permission(qs, permissions):
-            return _filter_generic_permission(qs, permissions, 'folder')
-
-        def filter_file_permission(qs, permissions):
-            return _filter_generic_permission(qs, permissions, 'file')
 
         # search
         q = request.GET.get('q', None)
@@ -333,8 +299,9 @@ class FolderAdmin(PrimitivePermissionAwareModelAdmin):
 
         user = request.user
         permissions = get_user_read_permissions(user)
-        folder_qs = filter_folder_permission(folder_qs, permissions)
-        file_qs = filter_file_permission(file_qs, permissions)
+        if permissions != 'All':
+            folder_qs = folder_qs.filter(Q(id__in=permissions) | Q(owner=request.user))
+            file_qs = file_qs.filter(Q(folder__id__in=permissions) | Q(owner=request.user))
         folder_children += folder_qs
         folder_files += file_qs
 
