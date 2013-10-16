918710ea534bf6dcbec231934291b97433a559d0
Laura Feier <laura.feier@3pillarglobal.com>
2013-10-16 17:05:32 +0300

    * allow make folder in root for site admins

--- filer/models/virtualitems.py
+++ filer/models/virtualitems.py
@@ -5,6 +5,7 @@ from django.utils.translation import ugettext_lazy as _
 from filer.models import mixins
 from filer.models.filemodels import File
 from filer.models.foldermodels import Folder
+from filer.utils.cms_roles import *
 
 
 class DummyFolder(mixins.IconsMixin):
@@ -99,3 +100,6 @@ class FolderRoot(DummyFolder):
 
     def get_admin_directory_listing_url_path(self):
         return urlresolvers.reverse('admin:filer-directory_listing-root')
+
+    def is_restricted_for_user(self, user):
+        return not has_admin_role(user)
