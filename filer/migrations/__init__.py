f6190513472cf377e61c220aa9bfefcd1b15f208
Vladimir Lagunov <lvo@albatrosscom.ru>
2013-11-14 11:38:30 +0700

    fixed User model in migrations

---
+++ filer/migrations/__init__.py
@@ -0,0 +1,11 @@
+try:
+    from django.contrib.auth import get_user_model
+except ImportError: # django < 1.5
+    from django.contrib.auth.models import User
+else:
+    User = get_user_model()
+
+user_orm_label = '%s.%s' % (User._meta.app_label, User._meta.object_name)
+user_model_label = '%s.%s' % (User._meta.app_label, User._meta.module_name)
+user_model_name = User.__name__
+user_db_table = User._meta.db_table
