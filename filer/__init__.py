8bda407a49733e152d360576f43bc3abe1b98f14
Stefan Foulis <stefan@foulis.ch>
2013-12-22 18:29:41 +0100

    bumps version to 0.9.6a1.dev3.perms

--- filer/__init__.py
+++ filer/__init__.py
@@ -1,3 +1,3 @@
 #-*- coding: utf-8 -*-
 # version string following pep-0396 and pep-0386
-__version__ = '0.9.6a1.dev2.perms'  # pragma: nocover
+__version__ = '0.9.6a1.dev3.perms'  # pragma: nocover
