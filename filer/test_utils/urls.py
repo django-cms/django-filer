85e4e4f1150f72dd62fc6cb0240e64642c4a5885
Iacopo Spalletti <i.spalletti@nephila.it>
2015-02-04 06:54:55 +0100

    Add media url to test urls

--- filer/test_utils/urls.py
+++ filer/test_utils/urls.py
@@ -1,10 +1,13 @@
 #-*- coding: utf-8 -*-
+from django.conf import settings
 from django.conf.urls import patterns, url, include
 from django.contrib import admin
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve',  # NOQA
+        {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('filer.server.urls')),
 )
