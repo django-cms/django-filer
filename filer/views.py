20cc7f84156a18cbb7281febbda26e485dc9fcb4
Joshua "jag" Ginsberg <jag@flowtheory.net>
2015-03-03 18:38:24 -0500

    Update views.py

--- filer/views.py
+++ filer/views.py
@@ -109,7 +109,7 @@ def make_folder(request, folder_id=None):
                 new_folder.owner = request.user
                 new_folder.save()
                 return render_to_response('admin/filer/dismiss_popup.html',
-                                          context_instance=RequestContext(request)))
+                                          context_instance=RequestContext(request))
     else:
         new_folder_form = NewFolderForm()
     return render_to_response('admin/filer/folder/new_folder_form.html', {
