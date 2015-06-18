8bebb2611d0af4159cc82e57edc39f24f37c999c
Adelina Diacu <adelina.diacu@3pillarglobal.com>
2015-06-18 16:06:53 +0300

    LUN-2309: updates on edit, delete pages

--- filer/templatetags/filer_admin_tags.py
+++ filer/templatetags/filer_admin_tags.py
@@ -41,6 +41,28 @@ def admin_css_base():
 def admin_js_base():
     return ADMIN_JS_BASE
 
+@register.inclusion_tag('admin/filer/submit_line.html', takes_context=True)
+def submit_row(context):
+    """
+    Displays the row of buttons for delete and save.
+    """
+    opts = context['opts']
+    change = context['change']
+    is_popup = context['is_popup']
+    save_as = context['save_as']
+    return {
+        'onclick_attrib': (opts.get_ordered_objects() and change
+                            and 'onclick="submitOrderForm();"' or ''),
+        'show_delete_link': (not is_popup and context['has_delete_permission']
+                              and (change or context['show_delete'])),
+        'show_save_as_new': not is_popup and change and save_as,
+        'show_save_and_add_another': context['has_add_permission'] and
+                            not is_popup and (not save_as or context['add']),
+        'show_save_and_continue': not is_popup and context['has_change_permission'],
+        'is_popup': is_popup,
+        'show_save': True
+    }
+
 
 @register.simple_tag(takes_context=True)
 def get_popup_params(context, sep='?'):
