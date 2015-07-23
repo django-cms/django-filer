32037f9de66c04fc631b2d4b9ce21ef832fb36dc
Adelina Diacu <adelina.diacu@3pillarglobal.com>
2015-07-23 15:31:21 +0300

    LUN-2309: remove deprecated function get_ordered_objects()

--- filer/templatetags/filer_admin_tags.py
+++ filer/templatetags/filer_admin_tags.py
@@ -50,18 +50,20 @@ def submit_row(context):
     change = context['change']
     is_popup = context['is_popup']
     save_as = context['save_as']
-    return {
-        'onclick_attrib': (opts.get_ordered_objects() and change
-                            and 'onclick="submitOrderForm();"' or ''),
-        'show_delete_link': (not is_popup and context['has_delete_permission']
-                              and (change or context['show_delete'])),
+    ctx = {
+        'opts': opts,
+        'show_delete_link': not is_popup and context['has_delete_permission'] and change and context.get('show_delete', True),
         'show_save_as_new': not is_popup and change and save_as,
-        'show_save_and_add_another': context['has_add_permission'] and
-                            not is_popup and (not save_as or context['add']),
+        'show_save_and_add_another': context['has_add_permission'] and not is_popup and (not save_as or context['add']),
         'show_save_and_continue': not is_popup and context['has_change_permission'],
         'is_popup': is_popup,
-        'show_save': True
+        'show_save': True,
+        'preserved_filters': context.get('preserved_filters'),
     }
+    if context.get('original') is not None:
+        ctx['original'] = context['original']
+    return ctx
+
 
 
 @register.simple_tag(takes_context=True)
