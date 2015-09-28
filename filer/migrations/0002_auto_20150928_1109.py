49fdf9b6fcec87651862eda0db36e8c3d0260dbb
Zoltan Szeredi <zoltan.szeredi@3pillarglobal.com>
2015-09-28 19:10:21 +0300

    Add migration 0002.

---
+++ filer/migrations/0002_auto_20150928_1109.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import django.db.models.deletion
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('filer', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='file',
+            name='owner',
+            field=models.ForeignKey(related_name='owned_files', on_delete=django.db.models.deletion.SET_NULL, verbose_name='owner', blank=True, to=settings.AUTH_USER_MODEL, null=True),
+        ),
+        migrations.AlterField(
+            model_name='folder',
+            name='shared',
+            field=models.ManyToManyField(help_text='All the sites which you share this folder with will be able to use this folder on their pages, with all of its assets. However, they will not be able to change, delete or move it, not even add new assets.', related_name='shared', verbose_name='Share folder with sites', to='sites.Site', blank=True),
+        ),
+    ]
