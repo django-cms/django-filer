1ba779adda5b514689fc037d2246ee0cfdb4d2c1
Martin Koistinen <mkoistinen@gmail.com>
2015-11-03 11:56:35 -0500

    Clean up

--- setup.py
+++ setup.py
@@ -1,3 +1,7 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
 from setuptools import setup, find_packages
 import os
 
@@ -8,32 +12,34 @@ except ImportError:
 
 version = __import__('filer').__version__
 
+
 def read(fname):
     # read the contents of a text file
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name = "django-filer",
-    version = version,
-    url = 'http://github.com/stefanfoulis/django-filer',
-    license = 'BSD',
+    name="django-filer",
+    version=version,
+    url='http://github.com/stefanfoulis/django-filer',
+    license='BSD',
     platforms=['OS Independent'],
-    description = "A file management application for django that makes handling of files and images a breeze.",
+    description="A file management application for django that makes handling "
+                "of files and images a breeze.",
     long_description = read('README.rst'),
-    author = 'Stefan Foulis',
-    author_email = 'stefan@foulis.ch',
+    author='Stefan Foulis',
+    author_email='stefan@foulis.ch',
     packages=find_packages(),
-    install_requires = (
-        # 'Django>=1.4,<1.5',  # no need to limit while in development
-        'Django>=1.4',
+    install_requires=(
+        'Django>=1.5',
         'easy-thumbnails>=1.0',
-        'django-mptt==0.5.2,==0.6,==0.6.1',
+        'django-mptt>=0.6',
         'django_polymorphic>=0.2',
     ),
     include_package_data=True,
     zip_safe=False,
-    classifiers = [
-        'Development Status :: 4 - Beta',
+    # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 5 - Production',
         'Framework :: Django',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
