0a46629511bd38b87e5cf3cd95f234ae788d1aa6
Evgeniy Negometyanov <evgeniy.negometyanov@divio.ch>
2015-11-19 11:52:32 +0200

    CMS-735: Implement FE part of sidebar image settings

---
+++ filer/static/filer/js/base.js
@@ -0,0 +1,14 @@
+// #####################################################################################################################
+// #BASE#
+// Basic logic django filer
+var Cl = window.Cl || {};
+/* global django, Mediator */
+
+(function ($) {
+    'use strict';
+
+    $(function () {
+        // mediator init
+        Cl.mediator = new Mediator();
+    });
+})(django.jQuery);
