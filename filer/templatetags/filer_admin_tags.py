b28f3c00225a43f8a2898cf30b1cf5209f7356de
Evgeniy Negometyanov <evgeniy.negometyanov@divio.ch>
2015-12-04 11:50:43 +0200

    Merge from new divio/django-filer repo to create pull-request to it.

--- filer/templatetags/filer_admin_tags.py
+++ filer/templatetags/filer_admin_tags.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
+from distutils.version import LooseVersion
 
+import django
 from django.template import Library
+
 
 register = Library()
 
@@ -13,3 +16,11 @@ def filer_actions(context):
     context['action_index'] = context.get('action_index', -1) + 1
     return context
 filer_actions = register.inclusion_tag("admin/filer/actions.html", takes_context=True)(filer_actions)
+
+
+@register.inclusion_tag('admin/filer/widgets/lookup.html', takes_context=True)
+def render_filer_lookup_button(context):
+    version = LooseVersion(django.get_version())
+    is_18_and_up = version >= LooseVersion('1.8')
+    context['IS_DJANGO_18'] = is_18_and_up and  version < LooseVersion('1.9')
+    return context
