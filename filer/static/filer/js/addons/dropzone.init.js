47759bc789761b545057dcc884de0a2a9a60a003
Loriana Indelicato <loriana_indelicato@hotmail.com>
2015-12-08 16:34:21 +0100

    add option to remove file

--- filer/static/filer/js/addons/dropzone.init.js
+++ filer/static/filer/js/addons/dropzone.init.js
@@ -12,7 +12,8 @@
             new Dropzone(dropzoneSelector, {
                 url: '/file/post',
                 paramName: 'file', // The name that will be used to transfer the file
-                maxFilesize: 2 // MB
+                maxFilesize: 2, // MB
+                addRemoveLinks: true
             });
         }
     });
