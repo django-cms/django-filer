3dd577a3f13de6fcd9bbba6573b6af4d3f69e003
Evgeniy Negometyanov <evgeniy.negometyanov@divio.ch>
2015-12-16 16:57:08 +0200

    Testing drag-and-drop with the new BE

--- filer/static/filer/js/addons/dropzone.init.js
+++ filer/static/filer/js/addons/dropzone.init.js
@@ -9,19 +9,30 @@
         var dropzone = $(dropzoneSelector);
         var dropzoneUrl = dropzone.data('url');
         var filerFile = $('.filerFile');
+        var fileIdInputSelector = $('#id_file');
 
         if (dropzone.length && Dropzone) {
             Dropzone.autoDiscover = false;
             new Dropzone(dropzoneSelector, {
-                url: dropzoneUrl,
+                url: dropzoneUrl + '?qqfile=userpic-500.jpg',
                 paramName: 'file', // The name that will be used to transfer the file
                 maxFilesize: 2, // MB
                 addRemoveLinks: true,
                 maxFiles: 1,
+                previewTemplate: $('.js-dz-template').html(),
                 clickable: false,
                 maxfilesexceeded: function (file) {
                     this.removeAllFiles();
                     this.addFile(file);
+                },
+                success: function (isSuccess, responce) {
+                    console.log(this);
+                    console.log(isSuccess);
+                    console.log(responce);
+
+                    if (isSuccess && responce && responce.preview) {
+                        $(fileIdInputSelector).val(responce.preview);
+                    }
                 },
                 drop: function () {
                     filerFile.hide();
