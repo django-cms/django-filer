0406b4a4552f5b353c886c60774e2486c1705c64
Evgeniy Negometyanov <evgeniy.negometyanov@divio.ch>
2016-01-06 14:45:32 +0200

    cropper tests

--- filer/tests/frontend/unit/test.cropper.js
+++ filer/tests/frontend/unit/test.cropper.js
@@ -2,13 +2,18 @@
 // #CROPPER TEST#
 
 'use strict';
-/* globals fixture, describe, it, expect, beforeEach, afterEach, Cl */
+/* globals fixture, describe, it, expect, beforeEach, afterEach, Cl, spyOn, jasmine */
 
 describe('Cl.Cropper', function () {
     var cropper;
     var container;
     var image;
     var location;
+    var preview;
+    var container2;
+    var image2;
+    var preview2;
+    var cropperStub;
 
     beforeEach(function () {
         fixture.setBase('frontend/fixtures');
@@ -17,7 +22,12 @@ describe('Cl.Cropper', function () {
         container = $('#container');
         image = $('#image');
         location = $('#location');
+        preview = $('#preview');
+        container2 = $('#container-2');
+        image2 = $('#image-2');
+        preview2 = $('#preview-2');
 
+        cropperStub = spyOn($.fn, 'cropper');
         cropper = new Cl.Cropper();
     });
 
@@ -29,10 +39,61 @@ describe('Cl.Cropper', function () {
         container = null;
         image = null;
         location = null;
+        preview = null;
+        container2 = null;
+        image2 = null;
+        preview2 = null;
+        cropperStub = null;
+        cropper = null;
     });
 
-    it('fake test', function () {
-        // TODO
-        expect('a').toBe('a');
+    it('does nothing if image was not loaded', function () {
+        cropper = new Cl.Cropper();
+        expect(cropperStub).not.toHaveBeenCalled();
+    });
+
+    it('cropper is loaded with correct params', function () {
+        cropper = new Cl.Cropper();
+        image.trigger('load');
+
+        expect(cropperStub).toHaveBeenCalled();
+        expect(cropperStub).toHaveBeenCalledWith({
+            viewMode: 2,
+            dragMode: 'crop',
+            autoCropArea: 1,
+            preview: preview,
+            built: jasmine.any(Function),
+            crop: jasmine.any(Function)
+        });
+    });
+
+    it('cropper plugin loads correctly', function (done) {
+        cropperStub.and.callThrough();
+        image.attr('src', '/img/blank.png');
+
+        cropper = new Cl.Cropper();
+
+        setTimeout(function () {
+            expect(cropperStub).toHaveBeenCalled();
+            expect(preview.children('img').length).toBe(1);
+            done();
+        }, 100);
+    });
+
+    it('mediator init works correctly', function () {
+        cropper = new Cl.Cropper();
+
+        Cl.mediator.publish('cropper:init', container2[0]);
+
+        image2.trigger('load');
+
+        expect(cropperStub).toHaveBeenCalledWith({
+            viewMode: 2,
+            dragMode: 'crop',
+            autoCropArea: 1,
+            preview: preview2,
+            built: jasmine.any(Function),
+            crop: jasmine.any(Function)
+        });
     });
 });
