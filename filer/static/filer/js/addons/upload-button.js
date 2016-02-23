e6edeb7f2e9c801977cad958e590519b97a84a34
Loriana Indelicato <loriana_indelicato@hotmail.com>
2016-02-23 10:05:56 +0100

    adds tooltip to disabled upload button

--- filer/static/filer/js/addons/upload-button.js
+++ filer/static/filer/js/addons/upload-button.js
@@ -114,7 +114,7 @@
             }, 1000);
         });
 
-        if (uploadButtonDisabled) {
+        if (uploadButtonDisabled.length) {
             uploadButtonDisabled.tooltip();
         }
     });
