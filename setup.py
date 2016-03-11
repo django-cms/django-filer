fd885c4e40a992f6a0bf46a88ef00e2643626e56
Stefan Foulis <stefan@foulis.ch>
2016-03-11 17:45:31 +0100

    update pypi classifiers

--- setup.py
+++ setup.py
@@ -47,9 +47,10 @@ setup(
         'Framework :: Django :: 1.6',
         'Framework :: Django :: 1.7',
         'Framework :: Django :: 1.8',
+        'Framework :: Django :: 1.9',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
     ],
 )
