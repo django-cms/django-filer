bb6d5a8b7f61e3e1d870b6a91264907f5bca7422
Mykhailo Kolesnyk <mykhailo.kolesnyk@divio.ch>
2016-03-30 12:46:35 +0300

    IconsMIxin: workaround for static() call on possibly non-existent file

--- filer/models/mixins.py
+++ filer/models/mixins.py
@@ -17,6 +17,14 @@ class IconsMixin(object):
         r = {}
         if getattr(self, '_icon', False):
             for size in FILER_ADMIN_ICON_SIZES:
-                r[size] = static("filer/icons/%s_%sx%s.png" % (
-                    self._icon, size, size))
+                try:
+                    r[size] = static("filer/icons/%s_%sx%s.png" % (
+                        self._icon, size, size))
+                except ValueError:
+                    # Do not raise an exception while trying to call static()
+                    # on non-existent icon file. This avoids the issue with
+                    # rendering parts of the template as empty blocks that
+                    # happens on an attempt to access object 'icons' attribute
+                    # in template.
+                    pass
         return r
