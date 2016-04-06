9b44d759eaaae9603e073f93edc15eb73629cefd
Loriana Indelicato <loriana_indelicato@hotmail.com>
2016-04-06 10:32:58 +0200

    linitng fixed

--- filer/static/filer/js/addons/copy-files.js
+++ filer/static/filer/js/addons/copy-files.js
@@ -1,12 +1,11 @@
 'use strict';
-/* global */
 
 jQuery(function ($) {
     var destinationOptions = $('#destination').find('option');
     var destinationOptionLength = destinationOptions.length;
     var submit = $('.js-submit');
 
-    if (destinationOptionLength == 1 && destinationOptions.prop('disabled')) {
+    if (destinationOptionLength === 1 && destinationOptions.prop('disabled')) {
         submit.prop('disabled', true);
     }
 });
