25dfc421337f925719d178ae2c54854ddd13007b
Loriana Indelicato <loriana_indelicato@hotmail.com>
2016-04-06 11:25:00 +0200

    rename js-submit class

--- filer/static/filer/js/addons/copy-files.js
+++ filer/static/filer/js/addons/copy-files.js
@@ -3,7 +3,7 @@
 jQuery(function ($) {
     var destinationOptions = $('#destination').find('option');
     var destinationOptionLength = destinationOptions.length;
-    var submit = $('.js-submit');
+    var submit = $('.js-submit-copy');
 
     if (destinationOptionLength === 1 && destinationOptions.prop('disabled')) {
         submit.prop('disabled', true);
