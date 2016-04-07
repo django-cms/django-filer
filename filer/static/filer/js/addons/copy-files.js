aa51f7c4a38dffecea58f9b44a725f79ab63213d
Loriana Indelicato <loriana_indelicato@hotmail.com>
2016-04-07 08:47:04 +0200

    adds back to button and tooltip

--- filer/static/filer/js/addons/copy-files.js
+++ filer/static/filer/js/addons/copy-files.js
@@ -10,8 +10,15 @@ django.jQuery(function ($) {
     var destinationOptions = $('#destination').find('option');
     var destinationOptionLength = destinationOptions.length;
     var submit = $('.js-submit-copy');
+    var tooltip = $('.js-disabled-btn-tooltip');
+    var tooltipText = tooltip.data('tooltip-text');
 
     if (destinationOptionLength === 1 && destinationOptions.prop('disabled')) {
         submit.prop('disabled', true);
+        tooltip.prop('title', tooltipText);
+        tooltip.attr('data-toggle', 'tooltip');
+        tooltip.attr('data-placement', 'bottom');
+debugger;
+        tooltip.tooltip();
     }
 });
