8a27bbcb8f5cfeee6295e80aba7b4689c30ae207
Loriana Indelicato <loriana_indelicato@hotmail.com>
2016-04-13 09:28:20 +0200

    fixes event target condition

--- filer/static/filer/js/base.js
+++ filer/static/filer/js/base.js
@@ -44,12 +44,12 @@ Cl.mediator = new Mediator();
 
         $('.js-filter-files').on('focus blur', function (event) {
             var container = $(this).closest('.js-filter-files-container');
-            var dropdownTrigger = container.find('.dropdown-container');
+            var dropdownTrigger = container.find('.dropdown-container a');
 
             if (event.type === 'focus') {
                 container.addClass('is-focused');
             } else {
-                if (event.relatedTarget === dropdownTrigger) {
+                if (!dropdownTrigger.is(event.relatedTarget)) {
                     container.removeClass('is-focused');
                 }
             }
