811ec8ba957a8ea66866c6760d5b49c6d6ce0001
Alex Hedlund <alex.hedlund@panic.fi>
2016-06-02 12:15:40 +0300

    Check FILER_IMAGE_MODEL setting before altering Image model in migrations

--- filer/migrations/0004_auto_20160328_1434.py
+++ filer/migrations/0004_auto_20160328_1434.py
@@ -3,6 +3,7 @@
 from __future__ import unicode_literals
 
 from django.db import migrations, models
+from filer.settings import FILER_IMAGE_MODEL
 
 
 class Migration(migrations.Migration):
@@ -11,10 +12,12 @@ class Migration(migrations.Migration):
         ('filer', '0003_thumbnailoption'),
     ]
 
-    operations = [
-        migrations.AlterField(
-            model_name='image',
-            name='subject_location',
-            field=models.CharField(blank=True, default=b'', max_length=64, verbose_name='subject location'),
-        ),
-    ]
+    operations = []
+    if not FILER_IMAGE_MODEL:
+        operations.append(
+            migrations.AlterField(
+                model_name='image',
+                name='subject_location',
+                field=models.CharField(blank=True, default=b'', max_length=64, verbose_name='subject location'),
+            ),
+        )
--- filer/migrations/0005_auto_20160429_1025.py
+++ filer/migrations/0005_auto_20160429_1025.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import migrations, models
+from filer.settings import FILER_IMAGE_MODEL
 
 
 class Migration(migrations.Migration):
@@ -11,10 +12,12 @@ class Migration(migrations.Migration):
         ('filer', '0004_auto_20160328_1434'),
     ]
 
-    operations = [
-        migrations.AlterField(
-            model_name='image',
-            name='subject_location',
-            field=models.CharField(blank=True, default='', max_length=64, verbose_name='subject location'),
-        ),
-    ]
+    operations = []
+    if not FILER_IMAGE_MODEL:
+        operations.append(
+            migrations.AlterField(
+                model_name='image',
+                name='subject_location',
+                field=models.CharField(blank=True, default='', max_length=64, verbose_name='subject location'),
+            ),
+        )
