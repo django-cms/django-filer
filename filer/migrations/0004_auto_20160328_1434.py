b8b3467f60ff4051340975c7eff9436ddf00a816
Martin Koistinen <mkoistinen@gmail.com>
2016-06-23 15:21:17 -0400

    Added a comment
    [ci skip]

--- filer/migrations/0004_auto_20160328_1434.py
+++ filer/migrations/0004_auto_20160328_1434.py
@@ -14,6 +14,7 @@ class Migration(migrations.Migration):
     ]
 
     if not FILER_IMAGE_MODEL:
+        # Only attempt to alter the Image field if we're actually using it.
         operations = [
             migrations.AlterField(
                 model_name='image',
