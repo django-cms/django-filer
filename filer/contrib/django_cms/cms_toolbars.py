a431df26c24b75e9788ff5889f0df78ce90c0ed9
Martin Koistinen <mkoistinen@gmail.com>
2016-06-27 20:01:13 -0400

    Fix flake issues

--- filer/contrib/django_cms/cms_toolbars.py
+++ filer/contrib/django_cms/cms_toolbars.py
@@ -7,7 +7,7 @@ from django.utils.encoding import force_text
 from django.utils.translation import ugettext_lazy as _
 
 from cms.cms_toolbars import ADMIN_MENU_IDENTIFIER, ADMINISTRATION_BREAK
-from cms.toolbar.items import Break, SideframeItem
+from cms.toolbar.items import Break
 from cms.toolbar_base import CMSToolbar
 from cms.toolbar_pool import toolbar_pool
 
@@ -23,8 +23,8 @@ class FilerToolbar(CMSToolbar):
     def get_insert_position(admin_menu, item_name):
         """
         Ensures that there is a SHORTCUTS_BREAK and returns a position for an
-        alphabetical position against all items between this break, and the
-        ADMINISTRATION_BREAK.
+        alphabetical position against all items between SHORTCUTS_BREAK, and
+        the ADMINISTRATION_BREAK.
         """
         start = admin_menu.find_first(Break, identifier=SHORTCUTS_BREAK)
         if not start:
@@ -35,8 +35,12 @@ class FilerToolbar(CMSToolbar):
 
         items = admin_menu.get_items()[start.index + 1: end.index]
         for idx, item in enumerate(items):
-            if force_text(item_name.lower()) < force_text(item.name.lower()):
-                return idx + start.index + 1
+            try:
+                if force_text(item_name.lower()) < force_text(item.name.lower()):
+                    return idx + start.index + 1
+            except AttributeError:
+                # Some item types do not have a 'name' attribute.
+                pass
         return end.index
 
     def populate(self):
