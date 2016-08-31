dd0e9834b07aae68e11773e547606fdeaec7da35
Paulo <commonzenpython@gmail.com>
2016-08-30 20:30:50 -0400

    Bumped version to 1.2.5rc1

--- filer/__init__.py
+++ filer/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
 # version string following pep-0396 and pep-0386
-__version__ = '1.2.4.post1'  # pragma: nocover
+__version__ = '1.2.5rc1'  # pragma: nocover
 
 default_app_config = 'filer.apps.FilerConfig'
