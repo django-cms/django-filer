0037ecb732976d6f50535b8f3df01ee1338ec7a8
Vladislav Manchev <blablacio@users.noreply.github.com>
2017-05-23 17:37:29 +0200

    Fix issue with inlines
    When dynamically adding dropzones (i.e. through inlines) they don't get initialized. This is caused by fix in baa8d52b31cf16d2145a2a8d9328ed74692dd361. Tested against Django 1.11.1 only.

--- filer/static/filer/js/addons/dropzone.init.js
+++ filer/static/filer/js/addons/dropzone.init.js
@@ -147,8 +147,8 @@ django.jQuery(function ($) {
             Dropzone.autoDiscover = false;
         }
         dropzones.each(createDropzone);
-        $('.add-row a').on('click', function () {
-            var dropzones = $(dropzoneSelector);
+        $(document).on('formset:added', function (ev, row) {
+            var dropzones = $(row).find(dropzoneSelector);
             dropzones.each(createDropzone);
         });
     }
