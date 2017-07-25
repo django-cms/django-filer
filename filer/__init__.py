9e035f05a2df345674362b38130c7c3a2d4800cc
Stefan Foulis <stefan@foulis.ch>
2017-07-25 13:42:19 +0200

    Bump develop version to 1.3.0.dev

--- filer/__init__.py
+++ filer/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
 # version string following pep-0396 and pep-0386
-__version__ = '1.2.9.dev'  # pragma: nocover
+__version__ = '1.3.0.dev'  # pragma: nocover
 
 default_app_config = 'filer.apps.FilerConfig'
