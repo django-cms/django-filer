a170eb7073a04f783e809035340c89239f08bc2e
Tadas Dailyda <tadas@dailyda.com>
2017-07-26 20:25:13 +0300

    fix tests

--- filer/tests/models.py
+++ filer/tests/models.py
@@ -252,7 +252,7 @@ class FilerApiTests(TestCase):
         Check that the correct model is loaded and save / reload data
         """
         image = self.create_filer_image()
-        if getattr(settings, 'FILER_IMAGE_MODEL', False):
+        if Image._meta.swapped:
             self.assertTrue(hasattr(image, 'extra_description'))
             self.assertFalse(hasattr(image, 'author'))
             image.extra_description = 'Extra'
