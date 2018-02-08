1ac82e54c5f7d58740ac99bb8f0443f697cea3ac
Russell Moffitt <russmo@coralhead.com>
2018-02-08 12:47:43 -0800

    using django.utils.functional keep_lazy over deprecated allow_lazy

--- filer/utils/compatibility.py
+++ filer/utils/compatibility.py
@@ -5,7 +5,10 @@ import sys
 
 import django
 from django.utils import six
-from django.utils.functional import allow_lazy
+try:
+    from django.utils.functional import keep_lazy as allow_lazy
+except ImportError:
+    from django.utils.functional import allow_lazy
 from django.utils.text import Truncator
 
 
