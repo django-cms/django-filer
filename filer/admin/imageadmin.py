5a60fbfbe82fcdd6dc4aadeeb4f04d1fbd23b57b
Duarte Fernandes <duartefq@outlook.com>
2018-03-14 15:50:33 -0300

    Fix import order (using isort, so the flake8 test passes)

--- filer/admin/imageadmin.py
+++ filer/admin/imageadmin.py
@@ -2,8 +2,9 @@
 from __future__ import absolute_import
 
 from django import forms
+from django.utils.translation import string_concat
 from django.utils.translation import ugettext as _
-from django.utils.translation import string_concat, ugettext_lazy
+from django.utils.translation import ugettext_lazy
 
 from ..settings import FILER_IMAGE_MODEL
 from ..thumbnail_processors import normalize_subject_location
