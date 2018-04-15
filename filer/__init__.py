a325b8f681a88e2ad7adda9518d0973d877762a9
Iacopo Spalletti <i.spalletti@nephila.it>
2018-04-15 17:45:37 +0200

    Release 1.3.1

--- filer/__init__.py
+++ filer/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
 # version string following pep-0396 and pep-0386
-__version__ = '1.3.0'  # pragma: nocover
+__version__ = '1.3.1'  # pragma: nocover
 
 default_app_config = 'filer.apps.FilerConfig'
