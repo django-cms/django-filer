18868aa742308bef843377788b44d16c99ee5c1c
Paulo <commonzenpython@gmail.com>
2018-05-09 11:20:48 -0400

    Bumped to 2.0.0dev2

--- filer/__init__.py
+++ filer/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
 # version string following pep-0396 and pep-0386
-__version__ = '2.0.0.dev1'  # pragma: nocover
+__version__ = '2.0.0.dev2'  # pragma: nocover
 
 default_app_config = 'filer.apps.FilerConfig'
