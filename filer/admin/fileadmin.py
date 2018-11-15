f11aa44491f7aa23c53b4baf8326d24e6583733a
vThaian <b.plociennik@merixstudio.com>
2018-11-15 11:54:43 +0100

    Fix rendering canonical URL in change form

--- filer/admin/fileadmin.py
+++ filer/admin/fileadmin.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import
 
 from django import forms
 from django.http import HttpResponseRedirect
+from django.utils.safestring import mark_safe
 from django.utils.translation import ugettext as _
 
 from .. import settings
@@ -142,7 +143,7 @@ class FileAdmin(PrimitivePermissionAwareModelAdmin):
     def display_canonical(self, instance):
         canonical = instance.canonical_url
         if canonical:
-            return '<a href="%s">%s</a>' % (canonical, canonical)
+            return mark_safe('<a href="%s">%s</a>' % (canonical, canonical))
         else:
             return '-'
     display_canonical.allow_tags = True
