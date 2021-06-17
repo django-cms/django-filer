29bd5783cc4974cf780c9a533ba818339c46903c
Jacob Rief <jacob.rief@uibk.ac.at>
2021-06-17 14:00:17 +0200

    use autocomplete_fields to access foreign keys

--- filer/admin/permissionadmin.py
+++ filer/admin/permissionadmin.py
@@ -1,26 +1,17 @@
 from django.contrib import admin
 
 from .. import settings
-from ..fields import folder
 
 
 class PermissionAdmin(admin.ModelAdmin):
-    fieldsets = (
-        (None, {'fields': (('type', 'folder', ))}),
-        (None, {'fields': (('user', 'group', 'everybody'), )}),
-        (None, {'fields': (
-            ('can_edit', 'can_read', 'can_add_children')
-        )}),
-    )
-    raw_id_fields = ['user', 'group']
+    fieldsets = [
+        (None, {'fields': ['type', 'folder']}),
+        (None, {'fields': ['user', 'group', 'everybody']}),
+        (None, {'fields': ['can_edit', 'can_read', 'can_add_children']}),
+    ]
+    autocomplete_fields = ['user', 'group']
     list_filter = ['user']
     list_display = ['__str__', 'folder', 'user']
-
-    def formfield_for_foreignkey(self, db_field, request, **kwargs):
-        db = kwargs.get('using')
-        if db_field.name == 'folder':
-            kwargs['widget'] = folder.AdminFolderWidget(db_field.remote_field, self.admin_site, using=db)
-        return super().formfield_for_foreignkey(db_field, request, **kwargs)
 
     def get_model_perms(self, request):
         # don't display the permissions admin if permissions are disabled.
