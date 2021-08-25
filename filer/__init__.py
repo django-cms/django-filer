bd37d9400b5ed432ea78c18e671748f26ec55738
Bogdan Cornianu <bogdan.cornianu@3pillarglobal.com>
2021-08-25 17:15:35 +0300

    Bump version

--- filer/__init__.py
+++ filer/__init__.py
@@ -1,5 +1,5 @@
 #-*- coding: utf-8 -*-
 # version string following pep-0396 and pep-0386
-__version__ = '0.9pbs.103'  # pragma: nocover
+__version__ = '0.9pbs.104'  # pragma: nocover
 
 default_app_config = 'filer.apps.FilerConfig'
