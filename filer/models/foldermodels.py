9301c97f31db9d20417d6b916123534ca5c68d42
Jacob Rief <jacob.rief@uibk.ac.at>
2022-04-07 13:15:22 +0200

    prepare for Django-4.0
    # Conflicts:
    #	filer/models/foldermodels.py

--- filer/models/foldermodels.py
+++ filer/models/foldermodels.py
@@ -8,8 +8,9 @@ from django.db.models import Q
 from django.urls import reverse
 from django.utils.functional import cached_property
 from django.utils.html import format_html, format_html_join
-from django.utils.http import urlquote
 from django.utils.translation import gettext_lazy as _
+
+from urllib.parse import quote as urlquote
 
 import mptt
 
