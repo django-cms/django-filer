f56fa458763958e367af3a4940031d08d6cc40ed
Jacob Rief <jacob.rief@gmail.com>
2023-05-10 11:12:19 +0200

    flake8 is complaining

--- filer/admin/folderadmin.py
+++ filer/admin/folderadmin.py
@@ -1,7 +1,6 @@
 import itertools
 import os
 import re
-from collections import OrderedDict
 from urllib.parse import quote as urlquote
 from urllib.parse import unquote as urlunquote
 
@@ -16,12 +15,11 @@ from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.db import models, router
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
-from django.urls import path, re_path, reverse
+from django.urls import path, reverse
 from django.utils.encoding import force_str
 from django.utils.html import escape, format_html
 from django.utils.safestring import mark_safe
-from django.utils.translation import gettext_lazy as _
-from django.utils.translation import ngettext_lazy
+from django.utils.translation import gettext_lazy as _, ngettext_lazy
 
 from .. import settings
 from ..models import File, Folder, FolderPermission, FolderRoot, ImagesWithMissingData, UnsortedImages, tools
@@ -204,34 +202,34 @@ class FolderAdmin(PrimitivePermissionAwareModelAdmin):
             path('',
                 self.admin_site.admin_view(self.directory_listing),
                 name='filer-directory_listing-root',
-            ),
+                ),
             path('last/',
                 self.admin_site.admin_view(self.directory_listing),
                 {'viewtype': 'last'},
                 name='filer-directory_listing-last',
-            ),
+                ),
             path('<int:folder_id>/list/',
                 self.admin_site.admin_view(self.directory_listing),
                 name='filer-directory_listing',
-            ),
+                ),
             path('<int:folder_id>/make_folder/',
                 self.admin_site.admin_view(views.make_folder),
                 name='filer-directory_listing-make_folder',
-            ),
+                ),
             path('make_folder/',
                 self.admin_site.admin_view(views.make_folder),
                 name='filer-directory_listing-make_root_folder',
-            ),
+                ),
             path('images_with_missing_data/',
                 self.admin_site.admin_view(self.directory_listing),
                 {'viewtype': 'images_with_missing_data'},
                 name='filer-directory_listing-images_with_missing_data',
-            ),
+                ),
             path('unfiled_images/',
                 self.admin_site.admin_view(self.directory_listing),
                 {'viewtype': 'unfiled_images'},
                 name='filer-directory_listing-unfiled_images',
-            ),
+                ),
         ] + super().get_urls()
 
     # custom views
