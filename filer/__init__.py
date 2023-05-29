c937d46154fd61a1995d58d1f9e0f6c5c950623f
Mihaela Nădăşan <mihaela.nadasan@3pillarglobal.com>
2023-05-29 11:40:31 +0300

    version increase for BEN-2516

--- filer/__init__.py
+++ filer/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 # version string following pep-0440
-__version__ = '0.9.113'  # pragma: nocover
+__version__ = '0.9.114'  # pragma: nocover
 
 default_app_config = 'filer.apps.FilerConfig'
