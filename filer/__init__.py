82dde3766315b21346b5f499288ea6ed60683398
Fabian Braun <fsbraun@gmx.de>
2023-06-11 16:20:03 +0200

    Bump to version 2.2.5

--- filer/__init__.py
+++ filer/__init__.py
@@ -13,6 +13,6 @@ Release logic:
  8. Publish the release and it will automatically release to pypi
 """
 
-__version__ = '2.2.4'
+__version__ = '2.2.5'
 
 default_app_config = 'filer.apps.FilerConfig'
