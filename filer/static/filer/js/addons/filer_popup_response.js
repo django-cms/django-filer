e2db798ebcd980fb7fd2a768c21a588f8090cf61
Fabian Braun <fsbraun@gmx.de>
2023-06-19 15:31:04 +0200

    fix js tests

--- filer/static/filer/js/addons/filer_popup_response.js
+++ filer/static/filer/js/addons/filer_popup_response.js
@@ -1,17 +1,20 @@
 /*global opener */
 (function () {
     'use strict';
-    var initData = JSON.parse(document.getElementById('django-admin-popup-response-constants').dataset.popupResponse);
-    switch (initData.action) {
-        case 'change':
-            // Specific function for file editing popup opened from widget
-            opener.dismissRelatedImageLookupPopup(window, initData.new_value, null, initData.obj, null);
-            break;
-        case 'delete':
-            opener.dismissDeleteRelatedObjectPopup(window, initData.value);
-            break;
-        default:
-            opener.dismissAddRelatedObjectPopup(window, initData.value, initData.obj);
-            break;
+    var dataElement = document.getElementById('django-admin-popup-response-constants');
+    if (dataElement) {
+        var initData = JSON.parse(dataElement.dataset.popupResponse);
+        switch (initData.action) {
+            case 'change':
+                // Specific function for file editing popup opened from widget
+                opener.dismissRelatedImageLookupPopup(window, initData.new_value, null, initData.obj, null);
+                break;
+            case 'delete':
+                opener.dismissDeleteRelatedObjectPopup(window, initData.value);
+                break;
+            default:
+                opener.dismissAddRelatedObjectPopup(window, initData.value, initData.obj);
+                break;
+        }
     }
 })();
