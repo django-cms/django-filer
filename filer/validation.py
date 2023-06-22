97bf760e1360b2ddd175fdb7445495bb1f42d221
Fabian Braun <fsbraun@gmx.de>
2023-06-22 23:46:25 +0200

    Comments and readability improvements

--- filer/validation.py
+++ filer/validation.py
@@ -1,19 +1,25 @@
+import typing
+
 from django.apps import apps
+from django.contrib.auth import get_user_model
 from django.utils.translation import gettext as _
+
+
+User = get_user_model()
 
 
 class FileValidationError(Exception):
     pass
 
 
-def deny_html(file_name, file, owner, mime_type):
+def deny_html(file_name: str, file: typing.IO, owner: User, mime_type: str) -> None:
     """Simple validator that denies all files"""
     raise FileValidationError(
         _('File "{}": HTML upload denied by site security policy').format(file_name)
     )
 
 
-def deny(file_name, file, owner, mime_type):
+def deny(file_name: str, file: typing.IO, owner: User, mime_type: str) -> None:
     file_type = file_name.rsplit(".")[-1]
     if file_type == file_name:
         raise FileValidationError(
@@ -24,7 +30,8 @@ def deny(file_name, file, owner, mime_type):
     )
 
 
-EVENT_ATTRIBUTES = (
+TRIGGER_XSS_THREAD = (
+    # Part 1: Event attributes that take js code as values
     # See https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/Events
     b"onbegin=", b"onend=", b"onrepeat=",
     b"onabort=", b"onerror=", b"onresize=", b"onscroll=", b"onunload=",
@@ -39,29 +46,34 @@ EVENT_ATTRIBUTES = (
     b"ontimeupdate=", b"ontoggle==", b"onvolumechange==", b"onwaiting=",
     b"onactivate=", b"onfocusin=", b"onfocusout=",
 ) + (
+    # Part 2:
     # Reject base64 obfuscated content
     b";base64,",
 ) + (
+    # Part 3: Obvious scripts
     # Reject direct <scrpit> tags or javascript: uri
     b"<script",
     b"javascript:",
 )
 
 
-def validate_svg(file_name, file, owner, mime_type):
+def validate_svg(file_name: str, file: typing.IO, owner: User, mime_type: str) -> None:
     """SVG files must not contain script tags or javascript hrefs.
     This might be too strict but avoids parsing the xml"""
     content = file.read().lower()
-    if any(map(lambda x: x in content, EVENT_ATTRIBUTES)):
+    if any(map(lambda x: x in content, TRIGGER_XSS_THREAD)):
+        # If any element of TRIGGER_XSS_THREAD is found in file, raise FileValidationError
         raise FileValidationError(
             _('File "{}": Rejected due to potential cross site scripting vulnerability').format(file_name)
         )
 
 
-def validate_upload(file_name, file, owner, mime_type) -> None:
+def validate_upload(file_name: str, file: typing.IO, owner: User, mime_type: str) -> None:
+    """Actual validation: Call all validators for the given mime type. The app config reads
+    the validators from the settings and replaces dotted paths by callables."""
+
     config = apps.get_app_config("filer")
 
-    for mt, validators in config.FILE_VALIDATORS.items():
-        if mime_type == mt:
-            for validator in validators:
-                validator(file_name, file, owner, mime_type)
+    if mime_type in config.FILE_VALIDATORS:
+        for validator in config.FILE_VALIDATORS[mime_type]:
+            validator(file_name, file, owner, mime_type)
