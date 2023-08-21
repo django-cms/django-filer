f5532518775c8c99dbced814676fe834824ae967
Corentin Bettiol <45763865+corentinbettiol@users.noreply.github.com>
2023-08-21 13:36:35 +0200

    fix: remove mentions of django 2.2 -> 3.2
    * update CHANGELOG.rst
    * remove Django 3.0 & 3.1 classifiers
    * update tests/requirements/frontend.txt

--- setup.py
+++ setup.py
@@ -31,8 +31,6 @@ CLASSIFIERS = [
     'Programming Language :: Python :: 3.10',
     'Programming Language :: Python :: 3.11',
     'Framework :: Django',
-    'Framework :: Django :: 3.0',
-    'Framework :: Django :: 3.1',
     'Framework :: Django :: 3.2',
     'Framework :: Django :: 4.0',
     'Framework :: Django :: 4.1',
