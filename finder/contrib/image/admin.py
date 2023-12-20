7245a483eaa3573f561c3c91e09e5e4552b16916
Jacob Rief <jacob.rief@gmail.com>
2023-12-20 14:40:10 +0100

    render EXIF header in image

--- finder/contrib/image/admin.py
+++ finder/contrib/image/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.forms.fields import CharField, FloatField, IntegerField
 from django.forms.widgets import HiddenInput, TextInput
+from django.utils.html import format_html, format_html_join
+from django.utils.translation import gettext_lazy as _
 
 from entangled.forms import EntangledModelForm
 
@@ -32,7 +34,30 @@ class ImageForm(EntangledModelForm):
 @admin.register(ImageModel)
 class ImageAdmin(FileAdmin):
     form = ImageForm
-    readonly_fields = ['owner', 'created_at', 'last_modified_at', 'summary', 'mime_type', 'sha1']
+    readonly_fields = ['details', 'owner', 'created_at', 'last_modified_at', 'mime_type', 'sha1']
+
+    def get_readonly_fields(self, request, obj=None):
+        readonly_fields = list(super().get_readonly_fields(request, obj))
+        if obj.mime_type in ['image/jpeg', 'image/webp']:
+            readonly_fields.append('exif')
+        return readonly_fields
+
+    @admin.display(description=_("Image details"))
+    def details(self, obj):
+        return obj.summary
+
+    @admin.display(description=_("EXIF-headers"))
+    def exif(self, obj):
+        if exif := obj.meta_data.get('exif'):
+            return format_html(
+                '<table>{}{}</table>',
+                format_html('<tr><th>{}</th><th>{}</th></tr>', _("Tag"), _("Value")),
+                format_html_join('',
+                    '<tr><td>{}</td><td>{}</td></tr>',
+                    ((key, value) for key, value in exif.items())
+                )
+            )
+        return '–'
 
     def get_settings(self, request, inode):
         settings = super().get_settings(request, inode)
