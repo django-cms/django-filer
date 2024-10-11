673aed3123f2aeb2c9a0a0b251071fe93dace541
Jacob Rief <jacob.rief@gmail.com>
2024-10-11 11:22:39 +0200

    use native video element rather than ReactPlayer

--- client/components/folderitem/Video.tsx
+++ client/components/folderitem/Video.tsx
@@ -5,30 +5,47 @@ import FigureLabels from 'finder/FigureLabels';
 export default function Video(props) {
 	const videoRef = useRef(null);
 
-	const onPlayPause = (isPlaying: boolean) => {
-		const player = videoRef.current.getInternalPlayer();
-		if (isPlaying) {
-			player.pause();
-			videoRef.current.seekTo(0);
-		} else {
-			player.play();
+	useEffect(() => {
+		if (!(videoRef.current?.parentElement instanceof HTMLElement))
+			return;
+		const wrapper = videoRef.current.parentElement;
+		let timeout = null;
+
+		const play = () => {
+			timeout && clearTimeout(timeout);
+			videoRef.current.play();
+		};
+
+		const pause = () => {
+			videoRef.current.pause();
+			timeout = setTimeout(() => {
+				// Reset the video to the beginning a second after leaving the video element
+				videoRef.current.currentTime = 0;
+			}, 1000);
+		};
+
+		wrapper.addEventListener('mouseenter', play);
+		wrapper.addEventListener('mouseleave', pause);
+
+		return () => {
+			videoRef.current?.pause();
+			timeout && clearTimeout(timeout);
+			wrapper.removeEventListener('mouseenter', play);
+			wrapper.removeEventListener('mouseleave', pause);
 		}
-	}
+	}, [videoRef]);
 
 	return (
 		<FigureLabels labels={props.labels}>{
 		props.sample_url ? (
-			<ReactPlayer
-				onMouseEnter={() => onPlayPause(false)}
-				onMouseLeave={() => onPlayPause(true)}
-				url={props.sample_url}
-				config={{attributes: {poster: props.thumbnail_url}}}
-				controls={false}
-				pip={false}
+			<video
 				ref={videoRef}
-				width="100%"
-				height="100%"
-			/>
+				src={props.sample_url}
+				poster={props.thumbnail_url}
+				controls={false}
+				preload="auto"
+				style={{width: '100%', height: '100%'}}
+			></video>
 		) : (
 			<img src={props.thumbnail_url} />
 		)
