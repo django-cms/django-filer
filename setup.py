911857576ab09d6536c3807429e9dd4d31aa66b1
Jacob Rief <jacob.rief@gmail.com>
2024-11-18 13:40:56 +0100

    adopt requirements to finder branch

--- setup.py
+++ setup.py
@@ -5,9 +5,8 @@ from filer import __version__
 
 
 REQUIREMENTS = [
-    'django>=3.2,<5.1',
-    'django-polymorphic',
-    'easy-thumbnails[svg]',
+    'django>=5.2',
+    'django-entangled',
 ]
 
 
@@ -26,22 +25,11 @@ CLASSIFIERS = [
     'Operating System :: OS Independent',
     'Programming Language :: Python',
     'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.8',
-    'Programming Language :: Python :: 3.9',
     'Programming Language :: Python :: 3.10',
     'Programming Language :: Python :: 3.11',
+    'Programming Language :: Python :: 3.12',
     'Framework :: Django',
-    'Framework :: Django :: 3.2',
-    'Framework :: Django :: 4.0',
-    'Framework :: Django :: 4.1',
-    'Framework :: Django :: 4.2',
-    'Framework :: Django CMS',
-    'Framework :: Django CMS :: 3.8',
-    'Framework :: Django CMS :: 3.9',
-    'Framework :: Django CMS :: 3.10',
-    'Framework :: Django CMS :: 3.11',
-    'Framework :: Django CMS :: 4.0',
-    'Framework :: Django CMS :: 4.1',
+    'Framework :: Django :: 5.2',
     'Topic :: Internet :: WWW/HTTP',
     'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     'Topic :: Software Development',
@@ -52,21 +40,21 @@ CLASSIFIERS = [
 setup(
     name='django-filer',
     version=__version__,
-    author='Divio AG',
-    author_email='info@divio.ch',
+    author='Django CMS Association and contributors',
+    author_email='info@django-cms.org',
     maintainer='Django CMS Association and contributors',
     maintainer_email='info@django-cms.org',
     url='https://github.com/django-cms/django-filer',
     license='BSD-3-Clause',
     description='A file management application for django that makes handling '
                 'of files and images a breeze.',
-    long_description=open('README.rst').read(),
+    long_description=open('README-Finder.md').read(),
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
     install_requires=REQUIREMENTS,
     extras_require=EXTRA_REQUIREMENTS,
-    python_requires='>=3.8',
+    python_requires='>=3.10',
     classifiers=CLASSIFIERS,
     test_suite='tests.settings.run',
 )
